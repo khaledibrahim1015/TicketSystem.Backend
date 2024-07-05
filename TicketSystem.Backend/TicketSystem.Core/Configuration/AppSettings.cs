51893a785957939d648365e948f5edd8956fdc3b
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-05 15:39:57 +0300

    add configuration file , and Models

---
+++ TicketSystem.Backend/TicketSystem.Core/Configuration/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Core.Configuration
+{
+    public  class AppSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
---
+++ TicketSystem.Backend/TicketSystem.Core/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Core.Models
+{
+    public  class Ticket
+    {
+        public int Id { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Governorate { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string District { get; set; } = string.Empty;
+
+        public bool IsHandled { get; set; } 
+
+
+    }
+}
