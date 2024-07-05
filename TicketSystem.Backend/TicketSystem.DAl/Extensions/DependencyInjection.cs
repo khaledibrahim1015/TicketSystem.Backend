dda30365aac8f4f01d06bdc34859738c26a69f4a
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-05 15:42:00 +0300

    in DAL => Adding ServiceCollectionsExtensions , DependencyInjecctions , add Factory . and Rquired packages

---
+++ TicketSystem.Backend/TicketSystem.DAl/Extensions/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketSystem.Core.Configuration;
+using TicketSystem.DAl.Factories;
+
+namespace TicketSystem.DAl.Extensions
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddServiceCollections (this IServiceCollection services , IConfiguration configuration)
+        {
+
+            // Configure AppSettings
+            services.Configure<AppSettings>(configuration.GetSection("AppSettings"));
+
+            // Register DbContext Using DbContextOptionsFactory 
+            services.AddSingleton<DbContextOptionsFactory>();
+
+            //  Register DbContext Using Factory 
+            services.AddScoped(serviceProvider =>
+            {
+                var OptionsFactory = serviceProvider.GetRequiredService<DbContextOptionsFactory>();
+                return new TicketDbContext(OptionsFactory.CreateDbContextOptions());
+
+            });
+
+            // Register other  services 
+            
+
+
+
+
+
+            return services;
+        }
+
+
+
+    }
+}
---
+++ TicketSystem.Backend/TicketSystem.DAl/Factories/DbContextOptionsFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystem.Core.Configuration;
+
+namespace TicketSystem.DAl.Factories
+{
+    public  class DbContextOptionsFactory
+    {
+        private readonly AppSettings _appSettings;
+
+        public DbContextOptionsFactory( IOptions<AppSettings> appSettings )
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        public DbContextOptions<TicketDbContext> CreateDbContextOptions()
+        {
+            DbContextOptionsBuilder<TicketDbContext> optionBuilder = new DbContextOptionsBuilder<TicketDbContext>();
+            optionBuilder.UseSqlServer(
+                                        _appSettings.ConnectionString );
+            return optionBuilder.Options;
+
+        }
+
+
+    }
+}
